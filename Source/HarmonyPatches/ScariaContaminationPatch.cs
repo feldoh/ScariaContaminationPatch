449e4637bde1e35a72339ad0a2b3c6d2689327f8
Dexter Lowe <feldoh10@gmail.com>
2022-10-06 11:50:06 +0100

    Prevent berserk on non-player pawns with scaria

--- Source/HarmonyPatches/ScariaContaminationPatch.cs
+++ Source/HarmonyPatches/ScariaContaminationPatch.cs
@@ -50,10 +50,74 @@ namespace ScariaContaminationPatch.HarmonyPatches
             // Berserk pawns wont attack people with scaria so we only need to see about cancelling if the attacker has it.
             if (pawn.IsColonistPlayerControlled ||
                 !(target is Pawn pawnTarget) ||
-                !MentalStateDefOf.Berserk.Equals(pawnTarget.MentalStateDef) ||
+                pawnTarget.def.race.intelligence < Intelligence.ToolUser ||
+                !(MentalStateDefOf.Berserk.Equals(pawnTarget.MentalStateDef) ||
+                  MentalStateDefOf.Manhunter.Equals(pawnTarget.MentalStateDef) ||
+                  MentalStateDefOf.ManhunterPermanent.Equals(pawnTarget.MentalStateDef)) ||
                 !pawn.health.hediffSet.HasHediff(HediffDefOf.Scaria))
                 return true;
+
+#if DEBUG
+            Log.Message($"Marked {pawnTarget.Name} as invalid target for {pawn.Name} as they are not a threat to this pawn");
+#endif
+            // We know the pawn must be non-player, have scaria, and the target must be berserk or manhunter so this is an invalid target.
+            __result = false;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(JobGiver_AIDefendPawn), "FindAttackTarget")]
+    public class PatchJobGiver_AIDefendPawn
+    {
+        public static bool Prefix(ref Thing __result, JobGiver_AIDefendPawn __instance,
+            bool ___attackMeleeThreatEvenIfNotHostile, Pawn pawn)
+        {
+            // Early exit as we don't want to do reflection unless we have to if this isn't set it'll be caught by the other patches
+            if (!___attackMeleeThreatEvenIfNotHostile) return true;
             
+            var defendee = Traverse.Create(__instance).Method("GetDefendee").GetValue<Pawn>();
+            if (!(pawn.IsColonistPlayerControlled ||
+                  !(defendee.mindState.meleeThreat is Pawn pawnTarget) ||
+                  pawnTarget.def.race.intelligence < Intelligence.ToolUser ||
+                  !(MentalStateDefOf.Berserk.Equals(pawnTarget.MentalStateDef) ||
+                    MentalStateDefOf.Manhunter.Equals(pawnTarget.MentalStateDef) ||
+                    MentalStateDefOf.ManhunterPermanent.Equals(pawnTarget.MentalStateDef)) ||
+                  !defendee.health.hediffSet.HasHediff(HediffDefOf.Scaria)))
+            {
+#if DEBUG
+                Log.Message("Reset melee threat");
+#endif
+                defendee.mindState.meleeThreat = null;
+            }
+
+            return true;
+        }
+    }
+
+
+    [HarmonyPatch(typeof(MentalStateHandler), nameof(MentalStateHandler.TryStartMentalState))]
+    public class PatchJobGiver_MentalStateHandler
+    {
+        public static bool Prefix(ref bool __result, Pawn ___pawn,
+            MentalStateDef stateDef,
+            string reason = null,
+            bool forceWake = false,
+            bool causedByMood = false,
+            Pawn otherPawn = null,
+            bool transitionSilently = false,
+            bool causedByDamage = false,
+            bool causedByPsycast = false)
+        {
+            Log.Message("Checking berserk permitted");
+            // Prevent scaria ridden non-player pawns going berserk
+            if (!MentalStateDefOf.Berserk.Equals(stateDef) ||
+                ___pawn.IsColonist ||
+                ___pawn.def.race.intelligence < Intelligence.ToolUser ||
+                !___pawn.health.hediffSet.HasHediff(HediffDefOf.Scaria)) return true;
+
+#if DEBUG
+            Log.Message("Preventing pawn from running berserk as they are a non-colonist with scaria");
+#endif
             __result = false;
             return false;
         }
