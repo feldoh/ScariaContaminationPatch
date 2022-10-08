4e535bb315130c6103f929a9e31e18b26d951a5f
Dexter Lowe <feldoh10@gmail.com>
2022-10-08 22:41:08 +0100

    Varied build working

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Source")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ScariaContaminationPatch")]
+[assembly: AssemblyDescription("Scaria Contamination Patch for Rimworld")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Source")]
