6587ecd793c53f08e34b337b2c25cb1039f0639d
Dexter Lowe <feldoh10@gmail.com>
2023-10-29 19:24:22 +0000

    No DLC fix

--- 1.4/Source/ScariaZombieDefOf.cs
+++ 1.4/Source/ScariaZombieDefOf.cs
@@ -6,6 +6,7 @@ namespace ScariaContaminationPatch;
 [DefOf]
 public static class ScariaZombieDefOf
 {
+    [MayRequireBiotech]
     public static GeneDef Taggerung_SCP_ScariaCarrier;
 
     static ScariaZombieDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(ScariaZombieDefOf));
