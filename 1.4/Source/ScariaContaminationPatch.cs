1ed491ca7214f516a221d1b201f4fea6ba32f348
Dexter Lowe <feldoh10@gmail.com>
2023-11-02 12:23:16 +0000

    Fixed

--- 1.4/Source/ScariaContaminationPatch.cs
+++ 1.4/Source/ScariaContaminationPatch.cs
@@ -4,15 +4,16 @@ using Verse;
 
 namespace ScariaContaminationPatch
 {
-    public class ScariaContaminationPatch: Mod
+    public class ScariaContaminationPatch : Mod
     {
         public static ScariaContaminationPatchSettings Settings;
+
         public ScariaContaminationPatch(ModContentPack content) : base(content)
         {
             new Harmony("Garethp.RimWorld.ScariaContainmentPatch.main").PatchAll();
             Settings = GetSettings<ScariaContaminationPatchSettings>();
         }
-        
+
         public override void DoSettingsWindowContents(Rect inRect)
         {
             base.DoSettingsWindowContents(inRect);
