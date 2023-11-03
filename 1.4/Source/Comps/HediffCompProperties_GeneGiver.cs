033e7accf62d3e9247ba5655c358dbfb3ea00903
Dexter Lowe <feldoh10@gmail.com>
2023-11-03 00:01:42 +0000

    Balance changes

--- 1.4/Source/Comps/HediffCompProperties_GeneGiver.cs
+++ 1.4/Source/Comps/HediffCompProperties_GeneGiver.cs
@@ -1,10 +1,22 @@
 using System.Collections.Generic;
+using RimWorld;
 using Verse;
 
 namespace ScariaContaminationPatch.Comps;
 
 public class HediffCompProperties_GeneGiver : HediffCompProperties
 {
+    public List<XenotypeDef> xenoTypes = new();
     public List<GeneDef> possibleGenes = new();
+    public int maxGenesToGive = 10;
+    public int numGenesToGive = 1;
+    public bool applyEntireXenotype = false;
+
     public HediffCompProperties_GeneGiver() => compClass = typeof(HediffComp_GeneGiver);
+
+    public override void ResolveReferences(HediffDef parent)
+    {
+        base.ResolveReferences(parent);
+        xenoTypes?.ForEach(x => possibleGenes.AddRange(x.genes));
+    }
 }
--- 1.4/Source/Comps/HediffComp_GeneGiver.cs
+++ 1.4/Source/Comps/HediffComp_GeneGiver.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using RimWorld;
 using Verse;
@@ -22,7 +24,11 @@ public class HediffComp_GeneGiver : HediffComp
     {
         if (other.TryGetComp<HediffComp_GeneGiver>() is { } hc)
         {
-            Props.possibleGenes.AddRange(hc.Props.possibleGenes);
+            Props.possibleGenes.AddRange(hc.Props.possibleGenes.Except(Props.possibleGenes));
+            Props.xenoTypes.AddRange(hc.Props.xenoTypes.Except(Props.xenoTypes));
+            Props.maxGenesToGive = Math.Max(Props.maxGenesToGive, hc.Props.maxGenesToGive);
+            Props.numGenesToGive = Math.Min(Props.numGenesToGive + hc.Props.numGenesToGive, Props.maxGenesToGive);
+            Props.applyEntireXenotype = Props.applyEntireXenotype || hc.Props.applyEntireXenotype;
         }
 
         base.CompPostMerged(other);
@@ -38,18 +44,41 @@ public class HediffComp_GeneGiver : HediffComp
 
         base.CompPostTick(ref severityAdjustment);
         Pawn pawn = this.parent.pawn;
-        if (!pawn.Spawned
-            || (double)this.parent.Severity < 1
-            || Props.possibleGenes
-                .Where(g => !pawn.genes.HasGene(g)).RandomElementWithFallback() is not { } gene) return;
 
-        pawn.genes.AddGene(gene, true);
+        if (!pawn.Spawned
+            || (double)this.parent.Severity < 1) return;
+
+        int genesToGive = Math.Min(Props.numGenesToGive, Props.maxGenesToGive);
+        string description = "";
+        if (Props.applyEntireXenotype)
+        {
+            Stack<GeneDef> viableGenes = new(Props.possibleGenes
+                .Distinct()
+                .Except(pawn.genes.GenesListForReading.Select(g => g.def))
+                .InRandomOrder());
+            while (genesToGive > 0 && !viableGenes.EnumerableNullOrEmpty())
+            {
+                GeneDef gene = viableGenes.Pop();
+                pawn.genes.AddGene(gene, true);
+                description += $"\n\t- {gene.LabelCap}";
+            }
+        }
+        else
+        {
+            XenotypeDef xenotype = Props.xenoTypes.RandomElement();
+            pawn.genes.SetXenotype(xenotype);
+            description = xenotype.LabelCap;
+        }
+
         shouldRemove = true;
 
         if (!PawnUtility.ShouldSendNotificationAbout(pawn))
             return;
         TaggedString label = "ScariaContaminationPatch_LetterLabelGeneGiver".Translate();
-        TaggedString taggedString = "ScariaContaminationPatch_LetterTextGeneGiver".Translate(pawn.Named("PAWN"), (NamedArgument)gene.label);
+        string letterText = Props.applyEntireXenotype
+            ? "ScariaContaminationPatch_LetterTextGeneGiverFullXenotype"
+            : "ScariaContaminationPatch_LetterTextGeneGiver";
+        TaggedString taggedString = letterText.Translate(pawn.Named("PAWN"), (NamedArgument)description);
         taggedString = taggedString.AdjustedFor(pawn);
         TaggedString text = taggedString.CapitalizeFirst();
         Find.LetterStack.ReceiveLetter(label, text, LetterDefOf.NegativeEvent, (LookTargets)(Thing)pawn);
